b6de69b376f82fe3d53595209f4383a44037ea0b
Manasi Ganu <MGanu@slb.com>
2023-12-23 22:37:08 +0530

    initial setup

---
+++ src/app.js
@@ -0,0 +1,21 @@
+import dotenv from 'dotenv';
+import express from 'express';
+import userRouter from './routers/user.js';
+
+// CONFIGURATIONS
+dotenv.config();
+const app = express();
+
+const port = process.env.PORT;
+
+app.use(express.json()); // for parsing application/json
+app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+app.use(userRouter);
+
+app.get('/*', (req, res) => {
+    res.render('404');
+});
+
+app.listen(port, function () {
+    console.log('Server started on port ' + port);
+});
---
+++ src/routers/user.js
@@ -0,0 +1,9 @@
+import express from 'express';
+
+const router = new express.Router();
+
+router.get(" ", (req,res) => {
+    console.log("minsi")
+}) 
+
+export default router
